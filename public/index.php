da083cda876fed6c70cce586801b1a5458c8dc78
Saeed Sheikholeslami <saeedsh.dev@gmail.com>
2018-04-05 13:22:38 +0430

    add router files
    router files and directories

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
 
-echo "MVC Framework";
+require "../vendor/autoload.php";
+
+$router = new \Core\Router;
+
+$router->add("series", ["use" => "SeriesController@index"]);
+$router->add("series/learning-php", "SeriesController@serie");
+$router->add("series/learning-php/episode/3", "SeriesController@episode");
+
+$url = $_SERVER['QUERY_STRING'];
+
+echo "Request URL : " . $url . "<br>";
+
+echo "<pre>";
+var_dump($router->getRoutes());
+echo "</pre>";
+
+if ($router->match($url)) {
+    echo "<pre>";
+    var_dump($router->getParams());
+    echo "</pre>";
+} else {
+    echo "no route found for url {$url}";
+}
