044a0714ab28530608e2d35dfec214a41deaa2a9
Saeed Sheikholeslami <saeedsh.dev@gmail.com>
2018-04-09 16:33:21 +0430

    finishing-routes
    working on routers and regex

--- public/index.php
+++ public/index.php
@@ -3,23 +3,10 @@
 require "../vendor/autoload.php";
 
 $router = new \Core\Router;
-
-//$router->add("/series", ["use" => "SeriesController@index"]);
-//$router->add("/series/{slug}", "SeriesController@serie");
+$router->add("/series", ["use" => "SeriesController@index"]);
+$router->add("/series/{slug}", "SeriesController@serie");
 $router->add("/series/{slug}/episode/{id}", "SeriesController@episode");
 
 $url = $_SERVER['QUERY_STRING'];
 
-echo "Request URL : " . $url . "<br>";
-
-echo "<pre>";
-var_dump($router->getRoutes());
-echo "</pre>";
-
-if ($router->match($url)) {
-    echo "<pre>";
-    var_dump($router->getParams());
-    echo "</pre>";
-} else {
-    echo "no route found for url {$url}";
-}
+$router->dispatch($url);
